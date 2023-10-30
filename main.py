ea1fce8487510f3d9fd8a9d69c30b0a8dad0789c
chernozem228338 <brawler.yt@mail.ru>
2023-10-31 00:12:34 +0300

    vk photo

--- main.py
+++ main.py
@@ -2,16 +2,16 @@ import vk_api
 import requests
 
 
-session = vk_api.VkApi(token='vk1.a.7fauLrHPcoxqNsV-m5bmGvnV-_qlPtJHbinpjnu2PPXOyigNOQFUXIILVPsdZMZ4-s5R7nGXlbKE8pggQCLEDe7QT1bFKoRaqVsn7PHlfjiq9Odvi9qUnp9UAy6GovdX6n_cJ7FbCPWDSUH_lirXiSUYA_EQQQnntkvmiUqxm11KvSBHDZ_0kXAzHtbrXo9SKJalbCTu0pkF7EZON1GWhw')
+session = vk_api.VkApi(token='token')
 vk = session.get_api()
 def upload_photo_to_group_wall(photo):
-    upload_url = vk.photos.getWallUploadServer(group_id=221162187, album_id=292733933)['upload_url']
+    upload_url = vk.photos.getWallUploadServer(group_id='d', album_id='d')['upload_url']
     request = requests.post(upload_url, files={'file': open(photo, "rb")})
     save_wall_photo = vk.photos.saveWallPhoto(group_id=221162187,
                                               photo=request.json()['photo'],
                                               server=request.json()['server'],
                                               hash=request.json()['hash'])
     saved_photo = 'photo' + str(save_wall_photo[0]['owner_id']) + '_' + str(save_wall_photo[0]['id'])
-    vk.wall.post(owner_id=-221162187, attachments=saved_photo)
+    vk.wall.post(owner_id=-'ds', attachments=saved_photo)
     print('Фото успешно загружено')
 upload_photo_to_group_wall('файл с фото')
